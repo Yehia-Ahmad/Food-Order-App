6c9ed4f82f3ddfacc3373150427576a4d29befb9
Yehia Ahmed <yehia790200@gmail.com>
2022-08-18 15:33:57 +0200

    <msg>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 import Header from "./components/Layout/Header";
+import AvailableMeals from "./components/Meals/AvailableMeals";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <MealsSummary />
+        <AvailableMeals />
+      </main>
     </React.Fragment>
   );
 }
